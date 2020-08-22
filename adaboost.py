ed154418362ce93bdb4e457001010f1bd045ee1d
Rafi Gana <rafig93@gmail.com>
2020-08-22 20:07:59 +0300

    fixed errors

--- adaboost.py
+++ adaboost.py
@@ -39,6 +39,63 @@ class ScrollableWindow(QtWidgets.QMainWindow):
         exit(self.qapp.exec_()) 
 
 
+def plot_adaboost(X: np.ndarray,
+                  y: np.ndarray,
+                  clf=None,
+                  sample_weights: Optional[np.ndarray] = None,
+                  annotate: bool = False,
+                  ax: Optional[mpl.axes.Axes] = None) -> None:
+    """ Plot ± samples in 2D, optionally with decision boundary if model is provided. """
+
+    assert set(y) == {-1, 1}, 'Expecting response labels to be ±1'
+
+    if not ax:
+        fig, ax = plt.subplots(figsize=(5, 5), dpi=100)
+        fig.set_facecolor('white')
+
+    pad = 1
+    x_min, x_max = X[:, 0].min() - pad, X[:, 0].max() + pad
+    y_min, y_max = X[:, 1].min() - pad, X[:, 1].max() + pad
+
+    if sample_weights is not None:
+        sizes = np.array(sample_weights) * X.shape[0] * 100
+    else:
+        sizes = np.ones(shape=X.shape[0]) * 100
+
+    X_pos = X[y == 1]
+    sizes_pos = sizes[y == 1]
+    ax.scatter(*X_pos.T, s=sizes_pos, marker='+', color='red')
+
+    X_neg = X[y == -1]
+    sizes_neg = sizes[y == -1]
+    ax.scatter(*X_neg.T, s=sizes_neg, marker='.', c='blue')
+
+    if clf:
+        plot_step = 0.01
+        xx, yy = np.meshgrid(np.arange(x_min, x_max, plot_step),
+                             np.arange(y_min, y_max, plot_step))
+
+        Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+        Z = Z.reshape(xx.shape)
+
+        # If all predictions are positive class, adjust color map acordingly
+        if list(np.unique(Z)) == [1]:
+            fill_colors = ['r']
+        else:
+            fill_colors = ['b', 'r']
+
+        ax.contourf(xx, yy, Z, colors=fill_colors, alpha=0.2)
+
+    if annotate:
+        for i, (x, y) in enumerate(X):
+            offset = 0.05
+            ax.annotate(f'$x_{i + 1}$', (x + offset, y - offset))
+    ax.set_xlim(x_min+0.5, x_max-0.5)
+    ax.set_ylim(y_min+0.5, y_max-0.5)
+    ax.set_xlabel('$x_1$')
+    ax.set_ylabel('$x_2$')
+
+
 def make_toy_dataset(n: int = 100, random_seed: int = None) -> (np.ndarray, np.ndarray):
     """ Generate a toy dataset for evaluating AdaBoost classifiers
 
@@ -140,7 +197,7 @@ def plot_staged_adaboost(X, y, clf, iters=10):
 
         # Plot strong learner
         trunc_clf = truncate_adaboost(clf, t=i + 1)
-        _ = ax2.set_title(f'Strong learner at t={i + 1}\n\u03B1={clf.stump_weights[i]}\n\u03B5={clf.errors[i]}')
+        _ = ax2.set_title(f'Strong learner at t={i + 1}')
         plot_adaboost(
             X, y, trunc_clf, sample_weights=clf.sample_weights[i], annotate=False, ax=ax2)
 
